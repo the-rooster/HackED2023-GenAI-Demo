c08f04a9d639cd259eb0d7333e6395ad8ab04f8f
the-rooster <andrewmjculberson@gmail.com>
2023-12-12 13:25:30 -0700

    bug

--- backend/utils.py
+++ backend/utils.py
@@ -3,12 +3,4 @@ import docx2txt
 
 def convert_docx_to_txt(file):
     """Convert a docx file to a txt file."""
-    # create a temporary file
-    with tempfile.NamedTemporaryFile(suffix='.txt') as tmp:
-        # convert the docx file to a txt file
-        tmp.write(docx2txt.process(file))
-        # read the txt file
-        tmp.seek(0)
-        txt = tmp.read()
-    # return the txt file
-    return txt
+    return docx2txt.process(file)
